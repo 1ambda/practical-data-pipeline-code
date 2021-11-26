956010cd934f30e20d35ae54b655ca1682a24742
1ambda <1amb4a@gmail.com>
2021-11-26 09:23:13 +0900

    fix: Modified metastore docker build

--- _dockerfile/docker-metastore/scripts/entrypoint.sh
+++ _dockerfile/docker-metastore/scripts/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-#export HADOOP_CLASSPATH=${HADOOP_HOME}/share/hadoop/tools/lib/aws-java-sdk-bundle-1.11.375.jar:${HADOOP_HOME}/share/hadoop/tools/lib/hadoop-aws-3.2.0.jar
 export HADOOP_VERSION=2.10.1
 export METASTORE_VERSION=2.3.9
+export AWS_SDK_VERSION=1.11.271
+
 export JAVA_HOME=/usr/local/openjdk-8
+export HADOOP_CLASSPATH=/opt/hadoop-${HADOOP_VERSION}/share/hadoop/tools/lib/aws-java-sdk-bundle-${AWS_SDK_VERSION}.jar:/opt/hadoop-${HADOOP_VERSION}/share/hadoop/tools/lib/hadoop-aws-${HADOOP_VERSION}.jar
 
 /opt/apache-hive-${METASTORE_VERSION}-bin/bin/schematool -initSchema -dbType mysql
 /opt/apache-hive-${METASTORE_VERSION}-bin/bin/hive --service metastore
