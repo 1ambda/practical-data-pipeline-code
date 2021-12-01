442a65c41996e8b9c8253e2cf724debc278fe38a
1ambda <1amb4a@gmail.com>
2021-12-01 15:52:46 +0900

    fix: Modified HMS scripts

--- _dockerfile/docker-metastore/scripts/entrypoint.sh
+++ _dockerfile/docker-metastore/scripts/entrypoint.sh
@@ -7,5 +7,7 @@ export AWS_SDK_VERSION=1.11.271
 export JAVA_HOME=/usr/local/openjdk-8
 export HADOOP_CLASSPATH=/opt/hadoop-${HADOOP_VERSION}/share/hadoop/tools/lib/aws-java-sdk-bundle-${AWS_SDK_VERSION}.jar:/opt/hadoop-${HADOOP_VERSION}/share/hadoop/tools/lib/hadoop-aws-${HADOOP_VERSION}.jar
 
+sleep 10;
+
 /opt/apache-hive-${METASTORE_VERSION}-bin/bin/schematool -initSchema -dbType mysql || true;
 /opt/apache-hive-${METASTORE_VERSION}-bin/bin/hive --service metastore
