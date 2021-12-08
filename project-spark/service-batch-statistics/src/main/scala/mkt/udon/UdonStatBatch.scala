d19cae19ecf25d2802b8fb02255b00a6409b8493
1ambda <1amb4a@gmail.com>
2021-12-08 09:05:36 +0900

    feat: Add sample TSV

--- project-spark/service-batch-statistics/src/main/scala/mkt/udon/UdonStatBatch.scala
+++ project-spark/service-batch-statistics/src/main/scala/mkt/udon/UdonStatBatch.scala
@@ -14,11 +14,14 @@ object UdonStatBatch extends SparkBase {
   override def driver(session: SparkSession): Unit = {
     val config = Environment.getConfigOrThrow[UdonStatBatchConfig]()
 
-    val resourcePath = getClass.getClassLoader.getResource("ecommerce-event.csv").getPath
+    val resourcePath = getClass.getClassLoader.getResource("marketing_campaign.csv").getPath
     val df = session.read.format("csv")
       .option("inferSchema", "true")
       .option("header", "true")
+      .option("sep", "\t")
       .load(resourcePath)
+
+    df.show()
 
     logger.info("Hello Statistics")
   }
