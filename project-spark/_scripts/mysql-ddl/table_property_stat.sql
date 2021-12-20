dec71980a53095281c9f16aa09c05e2854b1b59a
1ambda <1amb4a@gmail.com>
2021-12-20 10:17:22 +0900

    fix: Modified col name and types

--- project-spark/_scripts/mysql-ddl/table_property_stat.sql
+++ project-spark/_scripts/mysql-ddl/table_property_stat.sql
@@ -2,11 +2,11 @@ CREATE TABLE pipeline.property_stat
 (
     property_id   BIGINT UNSIGNED NOT NULL,
     property_type VARCHAR(30)                        NOT NULL,
-    lat DOUBLE(9, 8) NOT NULL,
-    lng DOUBLE(9, 8) NOT NULL,
+    lat DOUBLE(40, 10) NOT NULL,
+    lng DOUBLE(40, 10) NOT NULL,
 
     count_review_all BIGINT UNSIGNED  NOT NULL,
-    score_reivew_all DOUBLE(10, 5) NOT NULL,
+    score_review_all DOUBLE(10, 5) NOT NULL,
 
     count_review BIGINT UNSIGNED  NOT NULL,
     count_sales BIGINT UNSIGNED  NOT NULL,
