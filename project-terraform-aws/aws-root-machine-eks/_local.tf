a9771bbc0661a1e3d85a502acc39ce427782967f
1ambda <1amb4a@gmail.com>
2022-02-05 11:18:40 +0900

    feat: Add empty EMR, EKS Project

---
+++ project-terraform-aws/aws-root-machine-eks/_local.tf
@@ -0,0 +1,9 @@
+locals {
+  environment_common = "common"
+  environment_development = "development"
+  environment_production = "production"
+
+  region_seoul = "ap-northeast-2"
+
+  team_data = "data"
+}
---
+++ project-terraform-aws/aws-root-machine-emr/_terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.1.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.71.0"
+    }
+  }
+
+  /**
+   * 테스팅 목적으로 Terraform Backend 를 사용하지 않습니다
+   */
+  backend "local" {
+    path = "../__tf_state/_aws-root-machine-emr/terraform.tfstate"
+  }
+}
+
